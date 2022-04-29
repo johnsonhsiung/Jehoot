9f6ab0065db9438b81d2c9c13013bcd2672b437c
Rohin Gopalakrishnan <rohinrohin@gmail.com>
2022-04-29 15:48:10 -0700

    minor API updates

--- frontend/pages/room/index.js
+++ frontend/pages/room/index.js
@@ -1,12 +1,69 @@
 import Image from 'next/image';
 import Head from 'next/head';
 import OtpInput from 'react-otp-input';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Avatar from 'react-avatar';
+import { useQuery } from 'react-query'
+import { API_URL } from '../../constants';
+
 
 
 
 export default function Room() {
+
+  const [admin, setAdmin] = useState(false);
+  const [adminGameCreated, setAdminGameCreated] = useState(false);
+  const [username, setUsername] = useState('')
+  const [gamePin, setGamePin] = useState(false);
+  const [gameId, setGameId] = useState(null);
+
+  useEffect(() => {
+    setGamePin(localStorage.getItem('gamePin'))
+    setAdmin(localStorage.getItem('admin'))
+    setAdminGameCreated(localStorage.getItem('adminGameCreated'))
+  }, [])
+
+  const { data: createGameResponse, refetch: createGameRefetch, isLoading: createGameLoading } = useQuery(
+    ['createGame'],
+    async () => {
+      const { data } = await axios({
+        url: `${API_URL}/api/game/create`,
+        method: 'POST',
+        data: { 'admin': username }
+      });
+      return data;
+    }, {
+      refetchOnWindowFocus: false, 
+      enabled: false
+    });
+
+    const { data: gameBoardResponse, refetch: refetchGameBoard, isLoading: gameBoardLoading } = useQuery(
+      ['fetchGameBoard'],
+      async () => {
+        const { data } = await axios({
+          url: `${API_URL}/api/game/board`,
+          method: 'GET',
+          data: { 'admin': username }
+        });
+        return data;
+      }, {
+        refetchOnWindowFocus: false, 
+        enabled: false
+      });
+
+  
+    const handleCreateGame = async () => {
+      if (!username) {
+        alert("Please enter a username to continue")
+      }
+      try {
+        response = await createGameRefetch();
+        setGameId(response.game_id)
+      } catch (error) {
+        alert(error)
+      }
+    }
+
 
   return (
     <>
@@ -47,11 +104,13 @@ export default function Room() {
                     </div>
                   </div>
 
-                  <input className='text-2xl p-2 mx-4 rounded-md' placeholder='Enter a username to join'></input>
+                  {adminGameCreated && <div className='text-slate-50 text-2xl text-center font-normal opacity-30 mb-2'>GamePin - {gamePin}</div>}
+
+                  <input className='text-2xl p-2 mx-4 rounded-md' placeholder='Enter your name'></input>
                   <div className='pb-6 mt-4'>
                     <button className='clicky-button font-bold' onClick={(e) => {
                     }}>
-                      <span>Join</span></button>
+                      <span>{admin ? "Create" : "Join"}</span></button>
                   </div>
 
 
