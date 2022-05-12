79e339f29eab8dc59f474b7f5a21e6b3594aee6f
XanderCode <hello@XanderCode.com>
2022-05-12 12:48:30 -0700

    question change  Please enter the commit message for your changes. Lines starting

--- server/questions.py
+++ server/questions.py
@@ -7,99 +7,99 @@ questions = {
             'used' : False
         },
         '200': {
-            'question': '2 * 3 + 4 = _',
-            'answers': ['5', '6', '7', '10'],
-            'answer': 3,
+            'question': '2 * 5 + 4 = _',
+            'answers': ['5', '6', '14', '10'],
+            'answer': 2,
             'used' : False
         },
         '300': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
-            'answer': 0,
+            'question': '2 + 6 * 8 / 2 = _',
+            'answers': ['15', '26', '27', '38'],
+            'answer': 1,
             'used' : False
         },
         '400': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
-            'answer': 0,
+            'question': '6 + 8 % 3 = _',
+            'answers': ['5', '2', '7', '8'],
+            'answer': 3,
             'used' : False
         }
     },
     'Computer Science': {
         '100': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
-            'answer': 0,
+            'question': 'which one is computer language',
+            'answers': ['English', 'Chinese', 'Java', 'Japan'],
+            'answer': 2,
             'used': False
         },
         '200': {
-            'question': '2 * 3 + 4 = _',
-            'answers': ['5', '6', '7', '10'],
-            'answer': 3,
+            'question': 'Which CS language is for frontend',
+            'answers': ['Java', 'JS', 'C++', 'English'],
+            'answer': 2,
             'used': False
         },
         '300': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
+            'question': 'When was Java invented ',
+            'answers': ['1991', '1995', '2020', '1992'],
             'answer': 0,
             'used': False
         },
         '400': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
-            'answer': 0,
+            'question': 'Which key word is from Java ',
+            'answers': ['component', 'class', 'const', 'pointer'],
+            'answer': 1,
             'used' :False
         }
     },
-    'History': {
+    'WORLD WAR I ': {
         '100': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
-            'answer': 0,
-            'used' :False
-        },
-        '200': {
-            'question': '2 * 3 + 4 = _',
-            'answers': ['5', '6', '7', '10'],
-            'answer': 3,
+            'question': 'World War I ended in?',
+            'answers': ['1925', '1918', '1920', '1915'],
+            'answer': 1,
             'used': False
         },
+        '200': {
+            'question': 'Whose death sparked World War I?',
+            'answers': ['Kaiser Wilhelm', 'Archbishop Ussher', 'Queen Victoria', 'Archduke Franz Ferdinand'],
+            'answer': 3,
+            'used' :False
+        },
         '300': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
-            'answer': 0,
+            'question': 'Which of these nations was neutral in World War I?',
+            'answers': ['Germany', 'Norway', 'Italy', 'England'],
+            'answer': 1,
             'used': False
         },
         '400': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
-            'answer': 0,
+            'question': 'Which of these ships was sunk by a German submarine?',
+            'answers': ['Arizona', 'Lusitania', 'Titanic', 'Andrea Doria'],
+            'answer': 1,
             'used': False
         }
     },
-    'English': {
+    'SJSU': {
         '100': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
+            'question': 'Where is SJSU located',
+            'answers': ['Santa Clara', 'San Jose', 'Sunnyvale', 'Los Angles'],
             'answer': 0,
             'used': False
         },
         '200': {
-            'question': '2 * 3 + 4 = _',
-            'answers': ['5', '6', '7', '10'],
-            'answer': 3,
-            'used': False
-        },
-        '300': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
-            'answer': 0,
+            'question': 'What is the mascot of SJSU?',
+            'answers': ['Lion', 'Sheep', 'Spartan', 'Barbarian'],
+            'answer': 2,
             'used' :False
         },
-        '400': {
-            'question': '2 + 3 = _',
-            'answers': ['5', '6', '7', '8'],
+        '300': {
+            'question': 'What is the Graduation rate of SJSU ___ ?',
+            'answers': ['57%', '1%', '100%', '47%'],
             'answer': 0,
+            'used': False
+        },
+        '400': {
+            'question': 'Who is president of SJSU?',
+            'answers': ['Johnny Depp', 'Susan Martin', 'Tom Holland', 'Lawrence J Fox'],
+            'answer': 2,
             'used' :False
         }
     }
